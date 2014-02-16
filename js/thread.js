adeb8f7f40ad846f9f7ab07ed1a969a7db8c9672
Alive Kuo <alegnadise@gmail.com>
2014-02-16 21:55:04 +0800

    Add text shadow to avoid darken background

--- js/thread.js
+++ js/thread.js
@@ -35,7 +35,7 @@
     this.instanceID = this.CLASS_NAME + _id;
     _id++;
     return '<div class="thread" id="' + this.instanceID + '">' +
-            '<div class="name"><button class="btn-sm btn btn-default" style="background-color: ' + bgcolor + ';">' + (this.config.tasks ? this.config.tasks[0].threadId : '') + ' <span class="glyphicon glyphicon-chevron-up"></span></button></div>' +
+            '<div class="name"><button class="btn-sm btn btn-default" style="background-color: ' + bgcolor + '; text-shadow: 0 0 3px #fff;">' + (this.config.tasks ? this.config.tasks[0].threadId : '') + ' <span class="glyphicon glyphicon-chevron-up"></span></button></div>' +
             '<div class="canvas" id="' + this.instanceID + '-canvas"></div>' +
             '</div>';
   };
